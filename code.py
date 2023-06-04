de5474e8a1cfde80046be2b3a3cfad355af7c4d1
linhda <doanhlinh09@gmail.com>
2023-06-05 00:38:50 +0700

    change topo host ip

--- code.py
+++ code.py
@@ -8,14 +8,14 @@ class MyTopo( Topo ):
 
         Topo.__init__( self )
         # add host
-        h1 = self.addHost('h1', ip='10.0.1.1', mac='00:00:00:00:00:01')
-        h2 = self.addHost('h2', ip='10.0.2.1', mac='00:00:00:00:00:02')
-        h3 = self.addHost('h3', ip='10.0.3.1', mac='00:00:00:00:00:03')
-        h4 = self.addHost('h4', ip='10.0.4.1', mac='00:00:00:00:00:04')
-        h5 = self.addHost('h5', ip='10.0.5.1', mac='00:00:00:00:00:05')
-        h6 = self.addHost('h6', ip='10.0.6.1', mac='00:00:00:00:00:06')
-        h7 = self.addHost('h7', ip='10.0.7.1', mac='00:00:00:00:00:07')
-        h8 = self.addHost('h8', ip='10.0.8.1', mac='00:00:00:00:00:08')
+        h1 = self.addHost('h1', ip='10.0.0.1/24', mac='00:00:00:00:00:01')
+        h2 = self.addHost('h2', ip='10.0.0.2/24', mac='00:00:00:00:00:02')
+        h3 = self.addHost('h3', ip='10.0.0.3/24', mac='00:00:00:00:00:03')
+        h4 = self.addHost('h4', ip='10.0.0.4/24', mac='00:00:00:00:00:04')
+        h5 = self.addHost('h5', ip='10.0.0.5/24', mac='00:00:00:00:00:05')
+        h6 = self.addHost('h6', ip='10.0.0.6/24', mac='00:00:00:00:00:06')
+        h7 = self.addHost('h7', ip='10.0.0.7/24', mac='00:00:00:00:00:07')
+        h8 = self.addHost('h8', ip='10.0.0.8/24', mac='00:00:00:00:00:08')
 
         # add switch
         s1 = self.addSwitch('s1')
